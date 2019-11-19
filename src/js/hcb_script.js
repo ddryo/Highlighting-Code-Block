23190b7f64801899b4a2da504a73e6a206729ebc
ddryo <31400297+ddryo@users.noreply.github.com>
2019-11-19 21:06:58 +0900

    CSSとスクリプト微調節

--- src/js/hcb_script.js
+++ src/js/hcb_script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Token customize
     const keyToken = document.querySelectorAll('.token.keyword');
-    for (const i = 0; i < keyToken.length; i++) {
+    for (let i = 0; i < keyToken.length; i++) {
         const elem = keyToken[i];
 
         const defArr = ['function', 'def', 'class'];
@@ -14,20 +14,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Line highlighter position.
     const lineHighlighter = document.querySelectorAll('.line-highlight');
-    for (const i = 0; i < lineHighlighter.length; i++) {
+    for (let i = 0; i < lineHighlighter.length; i++) {
         const elem = lineHighlighter[i];
         if (!elem.parentNode.classList.contains('line-numbers')) {
             const dataStart = elem.getAttribute('data-start');
-            let topPosEm = (dataStart - 1) * 1.6;
-            if (5 < dataStart) {
-                topPosEm = topPosEm - dataStart * 0.02;
-            }
-            if (10 < dataStart) {
-                topPosEm = topPosEm - dataStart * 0.01;
-            }
-            if (20 < dataStart) {
-                topPosEm = topPosEm - dataStart * 0.001;
-            }
+
+            let topPosEm = (dataStart - 1) * 1.5; //line-heightの値
+
+            // Line highlighterの位置がずれるので桁数に応じて微調節 -> ずれなくなった？
+            // if (5 < dataStart) {
+            //     topPosEm = topPosEm - dataStart * 0.02;
+            // }
+            // if (10 < dataStart) {
+            //     topPosEm = topPosEm - dataStart * 0.01;
+            // }
+            // if (20 < dataStart) {
+            //     topPosEm = topPosEm - dataStart * 0.001;
+            // }
+
             elem.style.top = topPosEm + 'em';
         }
     }
