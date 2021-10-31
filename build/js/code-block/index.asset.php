c57d3eb8420dd585f02341db21cf848e88d6c50e
ddryo <dd.bocsfcryo@gmail.com>
2021-10-31 12:33:42 +0900

    buildファイル削除

--- build/js/code-block/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '3b403d8427d0440072b1a0359fa48718');
--- build/js/hcb_script.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'f1f87f0464c83d472ae1e202527aabf4');
